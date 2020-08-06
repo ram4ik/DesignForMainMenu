0755dc9236f07b9460987a52ef92b86366cb24be
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-06 14:26:56 +0300

    Initial Commit

---
+++ DesignForMainMenu/DesignForMainMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignForMainMenuApp.swift
+//  DesignForMainMenu
+//
+//  Created by ramil on 06.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct DesignForMainMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
